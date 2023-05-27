8cc8cf81e620f8f899a18d00c087874797125673
Arthur <arthurgomesferreira.agf@gmail.com>
2023-05-26 22:22:55 -0300

    Keymaps deleted

--- nvim/lua/arthur/core/keymaps.lua
+++ nvim/lua/arthur/core/keymaps.lua
@@ -1,13 +1,10 @@
-vim.g.mapleader = " "
+vim.g.mapleader = "<D->"
 
 -- for conciseness
 local keymap = vim.keymap
 
 
 -- general keymaps
-keymap.set("n","<leader>l",":nohl<CR>")
-keymap.set("n","<leader>+","<C-a>")
-keymap.set("n","<leader>-","<C-x>")
 keymap.set("n","<C-s>",":w<CR>")
 keymap.set("n","<C-x>",":q<CR>")
 keymap.set("n","<C-t>",":NERDTreeToggle<CR>")

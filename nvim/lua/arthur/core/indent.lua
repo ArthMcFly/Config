a80c8ad87825332049c84dc32fe68470028be193
Arthur <arthurgomesferreira.agf@gmail.com>
2023-06-03 09:24:52 -0300

    New indentation config

--- nvim/lua/arthur/core/indent.lua
+++ nvim/lua/arthur/core/indent.lua
@@ -1,11 +1,15 @@
 -- function ready to be executed when a file is saved
 function ExecuteOnSave()
+	-- save the current line
+	local current_position = vim.fn.getpos('.')
 	-- select the whole file 
 	vim.api.nvim_command("normal ggVG")
 	-- indent the file 
 	vim.api.nvim_command("normal =")
 	-- save the file
 	vim.api.nvim_command("w")
+	-- back up to the anterior line
+	vim.fn.setpos('.', current_position)
 end
 
 -- maps the BufWritePost event to call the function ExecuteOnSave()

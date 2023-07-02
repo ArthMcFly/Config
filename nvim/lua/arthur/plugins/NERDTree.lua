ca4318840490cf458c80efec1e0ee6758433939a
Arthur <arthurgomesferreira.agf@gmail.com>
2023-07-02 20:13:14 -0300

    NERDTree reconfigured

--- nvim/lua/arthur/plugins/NERDTree.lua
+++ nvim/lua/arthur/plugins/NERDTree.lua
@@ -1,3 +1,16 @@
 vim.g.NERDTreeShowHidden = 1
 --vim.cmd('autocmd VimEnter * NERDTree')
 
+vim.g.NERDTreeGitStatusIndicatorMapCustom = {
+	Modified  = "✹",
+	Staged    = "✚",
+	Untracked = "✭",
+	Renamed   = "➜",
+	Unmerged  = "═",
+	Deleted   = "✖",
+	Dirty     = "✗",
+	Clean     = "✔︎",
+	Ignored   = '☒',
+	Unknown   = "?"
+}
+
